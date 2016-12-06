968d93c25c452a313d8e94dbc284bcaee4d71ebb
chon <429215265@qq.com>
2016-12-06 11:42:08 +0800

    使用开源库

--- app/src/main/java/com/chon/runtimepermissiondemo/OriginalActivity.java
+++ app/src/main/java/com/chon/runtimepermissiondemo/OriginalActivity.java
@@ -2,66 +2,63 @@ package com.chon.runtimepermissiondemo;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.chon.runtimepermissiondemo.camera.CameraPreviewFragment;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
+/**
+ * 使用原生API
+ */
 public class OriginalActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_camera)
-    Button btnCamera;
-
-    private static final int REQUEST_CAMERA = 0;
+    private static final int REQUEST_CALLPHONE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_call_phone).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCamera();
+                callPhone();
             }
         });
     }
 
-    private void showCamera() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    private void callPhone() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED) {
-            // 应用没有授予相机权限，请求权限
+            // 应用没有授予拨打电话权限，请求权限
             requestCameraPermission();
         } else {
-            // 应用被授予相机权限 PackageManager.PERMISSION_GRANTED
-            showCameraPreview();
+            // 应用被授予拨打电话权限 PackageManager.PERMISSION_GRANTED
+            makeCall();
         }
     }
 
     private void requestCameraPermission() {
         // 如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
             // 向用户详细解释申请该权限的原因
             new AlertDialog.Builder(this)
                     .setCancelable(false)
-                    .setMessage("拍照需要使用相机权限,如果不授予权限会导致该功能无法正常使用")
+                    .setMessage("拨打电话需要使用电话权限,如果不授予权限会导致该功能无法正常使用")
                     .setPositiveButton("好的", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(OriginalActivity.this,
-                                    new String[]{Manifest.permission.CAMERA},
-                                    REQUEST_CAMERA);
+                            ActivityCompat.requestPermissions(
+                                    OriginalActivity.this,
+                                    new String[]{Manifest.permission.CALL_PHONE},
+                                    REQUEST_CALLPHONE
+                            );
                         }
                     })
                     .setNegativeButton("不给", new DialogInterface.OnClickListener() {
@@ -69,11 +66,12 @@ public class OriginalActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
                         }
-                    });
+                    })
+                    .show();
         } else {
             // 申请权限
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
-                    REQUEST_CAMERA);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALLPHONE);
         }
     }
 
@@ -83,10 +81,10 @@ public class OriginalActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
 
-        if (requestCode == REQUEST_CAMERA) {
+        if (requestCode == REQUEST_CALLPHONE) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 授予权限，打开相机
-                showCameraPreview();
+                // 授予权限，拨打电话
+                makeCall();
             } else {
                 Toast.makeText(this, "请求权限被拒绝", Toast.LENGTH_SHORT).show();
             }
@@ -95,11 +93,9 @@ public class OriginalActivity extends AppCompatActivity {
         }
     }
 
-    private void showCameraPreview() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frameLayout, CameraPreviewFragment.newInstance())
-                .addToBackStack("camera")
-                .commit();
+    private void makeCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:123456"));
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/chon/runtimepermissiondemo/PermissionsDispatcherActivity.java
@@ -0,0 +1,123 @@
+package com.chon.runtimepermissiondemo;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+/**
+ * 使用 PermissionsDispatcher
+ */
+@RuntimePermissions
+public class PermissionsDispatcherActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        findViewById(R.id.btn_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionsDispatcherActivityPermissionsDispatcher.takePhotoWithCheck(PermissionsDispatcherActivity.this);
+            }
+        });
+
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) { // 如果返回数据
+            if (requestCode == 100) { // 判断请求码是否为REQUEST_CAMERA,如果是代表是这个页面传过去的，需要进行获取
+                Bundle bundle = data.getExtras(); // 从data中取出传递回来缩略图的信息，图片质量差，适合传递小图片
+                Bitmap bitmap = (Bitmap) bundle.get("data"); // 将data中的信息流解析为Bitmap类型
+                imageView.setImageBitmap(bitmap);// 显示图片
+            }
+        }
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForRecord(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("好的", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("不给", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage("拍照需要相机权限，应用将要申请使用相机权限")
+                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showRecordDenied() {
+        Toast.makeText(getApplicationContext(), "权限被拒绝", Toast.LENGTH_LONG).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void onRecordNeverAskAgain() {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("好的", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 打开系统应用设置
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        intent.addCategory(Intent.CATEGORY_DEFAULT);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage("您已经禁止了相机权限,是否现在去开启")
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionsDispatcherActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+}
